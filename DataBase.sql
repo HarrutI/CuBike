bf60058275bca9c7b437850b9b37a7aada088327
Alejandro Parada <101281165+AlejandroParadaUCA@users.noreply.github.com>
2024-03-15 15:11:09 +0100

    Update DataBase.sql

--- DataBase.sql
+++ DataBase.sql
@@ -1,8 +1,8 @@
 -- Crear la base de datos
-CREATE DATABASE IF NOT EXISTS CuBike;
+CREATE DATABASE IF NOT EXISTS UnderBike;
 
 -- Usar la base de datos
-USE CuBike;
+USE UnderBike;
 
 -- Tabla de usuarios
 CREATE TABLE IF NOT EXISTS usuarios (
@@ -11,7 +11,8 @@ CREATE TABLE IF NOT EXISTS usuarios (
     apellido VARCHAR(50) NOT NULL,
     passsword VARCHAR(50) NOT NULL,
     roles ENUM('admin', 'user', 'banned') NOT NULL DEFAULT 'user',
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_subscripcion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- Crear la tabla estacion
 CREATE TABLE IF NOT EXISTS estacion (
@@ -20,7 +21,6 @@ CREATE TABLE IF NOT EXISTS estacion (
     capacidad INT NOT NULL,
     ubicacion VARCHAR(100) NOT NULL
 );
--- Tabla de bicicletas
 -- Tabla de bicicletas
 CREATE TABLE IF NOT EXISTS bicicletas (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,5 +35,6 @@ CREATE TABLE IF NOT EXISTS bicicletas (
 ALTER TABLE bicicletas
 ADD CONSTRAINT fk_estacion_id
 FOREIGN KEY (estacion_id) REFERENCES estacion(id);
+
 
 
